0e62ae55410f9abdad4d3e96e6fbeac199f3fdcb
roboseb <sebastienapril@rocketmail.com>
2022-04-17 23:44:13 -0600

    Add /images. Add function to generate books based on a book object. Add randomized art for book covers. Add book panel page generation based on book length. Add skeleton for add book input.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # library
 Library project for The Odin Project
+
+---Features---
+
+-
+
+
+---Devlog---
+
+April 17th, 2022
+
+	First full day of working on this library project. Had the basic layout and colour scheme set up already yesterday, but I immediately had the idea to make the books have more pages on screen based on the actual pagecounts entered. This probably would've been pretty easy to do with an absolutely positioned image under the page cover, and having that image just slide further out based on the size of the book. I decided to instead use divs and css to generate trapezoids that progressively get smaller, creating a sense of perspective on the book. Even if it's a stupider way to implement this overall, I think it'll still end up being good practice.
+	
+	Kinda tired today. Should be able to sleep well tonight and make some good progress on this tomorrow. Was still able to implement a decent looking book layout with randomized cover art. Also implemented the skeleton for the add book input panel pretty quickly. Should be easy enough to add functionality, as most of the pieces are all there. I'll probably limit the scope of the fancy stuff I add to this from here on out, as I want to speed through some more of TOP and really invest time in the bigger projects. We'll see.
+
+---To-Do---
+
+DONE-Scale binding to book size
+DONE-Add pages based on book size
+DONE-Generate book info on book
+-Input options for adding a new book
+-Clean up UI
+-make an amount of book equal to (fix rows) 
+DONE-randomize or generate book art?
+-Edit button for books
+-Make every other page jut out
+-bottom margin for books based on page count
+-SHADOWS
+-form verification for add book panel
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+const coverArt = ['images/face1.png', 'images/face2.png', 
+                    'images/rose.png', 'images/hand.png'];
 
 //Book constructor.
 function Book(title, author, pages, read) {
@@ -8,8 +10,14 @@ function Book(title, author, pages, read) {
     this.read = read 
     this.info = function() {
         let readAlert;
-        read ? readAlert = 'read.' : readAlert = 'not read yet.';
-        return `${title} by ${author}, ${pages}, ${readAlert}`;
+        read ? readAlert = 'read' : readAlert = 'not read yet';
+        return `${title} by ${author}, ${pages} pages, ${readAlert}`;
+    }
+    this.readAlert = function() {
+        let readAlert;
+        read ? readAlert = 'Read' : readAlert = 'Unread';
+        return readAlert;
+
     }
 }
 
@@ -18,24 +26,130 @@ function addBook(book) {
 }
 
 const hatchet = new Book('Hatchet', 'Gary Paulsen', 195, true);
+const test = new Book('The Lord of the Rings', 'Test Testsson', 50, false);
 
 addBook(hatchet);
-addBook(hatchet);
-console.log(myLibrary);
+addBook(test);
 
-const book = document.querySelector('.bookbox');
-console.log(book);
-console.log(book.firstElementChild.firstElementChild);
-//book.firstElementChild
 
-const pageBottom = book.firstElementChild.firstElementChild;
+//Returns last descendant of an element.
+function getLastDescendant(element) {
+    let descendants = Array.from(element.querySelectorAll('*'));
+    return descendants[descendants.length -1];
+}
 
-const newPage = document.createElement('div');
-newPage.classList.add('page');
-pageBottom.appendChild(newPage);
+//Adds top page to the bottom of a book.
+function addPage(firstPage) {
 
-const newPageBottom = book.firstElementChild.firstElementChild.firstElementChild;
+    let bottomPage = getLastDescendant(firstPage);
 
-const newNewPage = document.createElement('div');
-newNewPage.classList.add('page', 'pagebottom');
-newPageBottom.appendChild(newNewPage);
+    const newPage = document.createElement('div');
+    newPage.classList.add('page');
+    bottomPage.appendChild(newPage);
+}
+
+//Adds the lower page to the bottom of a book.
+function addPageBottom(firstPage) {
+
+    let bottomPage = getLastDescendant(firstPage);
+
+    const newPage = document.createElement('div');
+    newPage.classList.add('page', 'pagebottom');
+    bottomPage.appendChild(newPage);
+}
+
+//Add a new book to the book grid.
+function displayBook(book) {
+
+    const extraPages = Math.floor(book.pages/50);
+
+    //Add a book container to the book grid.
+    const bookGrid = document.getElementById('bookgrid');
+    const bookBox = document.createElement('div');
+    bookBox.classList.add('bookbox');
+    bookGrid.appendChild(bookBox);
+
+    //Generate base page and page bottom.
+    const firstPage = document.createElement('div');
+    firstPage.classList.add('page', 'firstpage');
+    bookBox.appendChild(firstPage);
+    const secondPage = document.createElement('div');
+    secondPage.classList.add('page', 'pagebottom');
+    firstPage.appendChild(secondPage);
+
+    //Generate book binding based on book page count.
+    const binding = document.createElement('div');
+    binding.classList.add('binding');
+    binding.style.bottom = `${-6 * (extraPages + 1)}px`;
+    binding.style.borderLeft = `${6 * (extraPages + 1)}px solid transparent`;
+    binding.style.borderTop = `${6 * (extraPages + 1)}px solid var(--accent)`;
+    bookBox.appendChild(binding);
+
+
+    //Generate book top.
+    const bookTop = document.createElement('div');
+    bookTop.classList.add('booktop');
+    bookBox.appendChild(bookTop);
+
+    //Add book cover.
+    const bookCover = document.createElement('img');
+    bookCover.src = coverArt[Math.floor(Math.random() * coverArt.length)];
+    bookTop.appendChild(bookCover);
+
+
+    //Display book info on cover, and generate relevant flexboxes.
+    const mainBox = document.createElement('div');
+    mainBox.classList.add('mainbox');
+    bookTop.appendChild(mainBox);
+
+    const bookTitle = document.createElement('div');
+    bookTitle.classList.add('bookinfo', 'booktitle');
+    bookTitle.innerText = book.title;
+    mainBox.appendChild(bookTitle);
+
+    const secondBox = document.createElement('div');
+    secondBox.classList.add('secondbox');
+    mainBox.appendChild(secondBox);
+
+    const bookAuthor = document.createElement('div');
+    bookAuthor.classList.add('bookinfo', 'bookauthor');
+    bookAuthor.innerText = book.author;
+    secondBox.appendChild(bookAuthor);
+
+    const thirdBox = document.createElement('div');
+    thirdBox.classList.add('thirdbox');
+    secondBox.appendChild(thirdBox);
+
+    const bookPages = document.createElement('div');
+    bookPages.classList.add('bookinfo', 'bookpages');
+    bookPages.innerText = `${book.pages} pages`;
+    thirdBox.appendChild(bookPages);
+
+    const bookRead = document.createElement('div');
+    bookRead.classList.add('bookinfo', 'bookread');
+    bookRead.innerText = book.readAlert();
+    thirdBox.appendChild(bookRead);
+    
+
+    //Display extra pages based on book page count.
+    for (let i = 0; i < extraPages; i++) {
+        addPage(firstPage);
+        addPageBottom(firstPage);
+    }
+}
+
+const addBookBtn = document.querySelector('#addbookbtn');
+addBookBtn.addEventListener('click', () => {
+    const title = prompt('What is the title?');
+    const author = prompt('Who is the author?');
+    const pages = prompt('How many pages are there?');
+    const read = prompt('Have you read the book?');
+    const fuck = new Book(title, author, pages, read); 
+    addBook(fuck);
+    displayBook(fuck);
+});
+
+myLibrary.forEach(book => {
+    displayBook(book);
+});
+
