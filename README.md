1175e6dc8670a9e8dcc4bebbe479aa5c1d3a99d4
roboseb <sebastienapril@rocketmail.com>
2022-04-18 19:32:08 -0600

    Add function to add custom books. Add basic structure to edit existing book.

--- README.md
+++ README.md
@@ -14,17 +14,27 @@ April 17th, 2022
 	
 	Kinda tired today. Should be able to sleep well tonight and make some good progress on this tomorrow. Was still able to implement a decent looking book layout with randomized cover art. Also implemented the skeleton for the add book input panel pretty quickly. Should be easy enough to add functionality, as most of the pieces are all there. I'll probably limit the scope of the fancy stuff I add to this from here on out, as I want to speed through some more of TOP and really invest time in the bigger projects. We'll see.
 
+April 18th, 2022
+
+	New keyboard and mouse came in way earlier than expected, so that's nice. Unfortunately the hard part will be editing books apparently. Adding a new book was a relatively simple process, but editing existing content is proving to be a huge pain in the ass. Thought I might be done with this project by the end of the day, but not so sure if that'll be the case anymore. It'll eventually work though, and by the time it does I'm sure the trouble will be worth it.
+
+	Wow, several hours later and still massively struggling, not sure what the issue is yet. Will have to take a break for now and come back tomorrow with a fresh set of eyes. I think I built this from the ground up in a way that would screw me over down the line. That or I don't understand objects yet. Probably the latter.
+
 ---To-Do---
 
 DONE-Scale binding to book size
 DONE-Add pages based on book size
 DONE-Generate book info on book
--Input options for adding a new book
+DONE-Input options for adding a new book
 -Clean up UI
 -make an amount of book equal to (fix rows) 
 DONE-randomize or generate book art?
 -Edit button for books
 -Make every other page jut out
--bottom margin for books based on page count
+CANCEL-bottom margin for books based on page count
 -SHADOWS
--form verification for add book panel
+DONE-form verification for add book panel
+-add 70s design squiggles and some big stylized numbers like in 70s architecture
+DONE-Input from hitting enter or hitting submit
+DONE-Limit pages added to bottom of book
+-delete book button
--- script.js
+++ script.js
@@ -17,7 +17,13 @@ function Book(title, author, pages, read) {
         let readAlert;
         read ? readAlert = 'Read' : readAlert = 'Unread';
         return readAlert;
+    }
+    this.display = function() {
+        displayBook(this);
+    }
 
+    this.delete = function() {
+        deleteBook(this);
     }
 }
 
@@ -25,43 +31,28 @@ function addBook(book) {
     myLibrary.push(book);
 }
 
-const hatchet = new Book('Hatchet', 'Gary Paulsen', 195, true);
-const test = new Book('The Lord of the Rings', 'Test Testsson', 50, false);
+const hatchet = new Book('Hatchet', 'Gary Paulsen', 4000, true);
+const testBook = new Book('Test Book', "Testson", 300, false)
 
 addBook(hatchet);
-addBook(test);
+addBook(testBook)
 
-
-//Returns last descendant of an element.
-function getLastDescendant(element) {
-    let descendants = Array.from(element.querySelectorAll('*'));
-    return descendants[descendants.length -1];
-}
-
-//Adds top page to the bottom of a book.
-function addPage(firstPage) {
-
-    let bottomPage = getLastDescendant(firstPage);
-
-    const newPage = document.createElement('div');
-    newPage.classList.add('page');
-    bottomPage.appendChild(newPage);
-}
-
-//Adds the lower page to the bottom of a book.
-function addPageBottom(firstPage) {
-
-    let bottomPage = getLastDescendant(firstPage);
-
-    const newPage = document.createElement('div');
-    newPage.classList.add('page', 'pagebottom');
-    bottomPage.appendChild(newPage);
+function deleteBook(book) {
+    const targets = Array.from(document.querySelectorAll('.booktitle'));
+    targets.forEach(target => {
+    
+        if (target.innerText.toLowerCase() === book.title.toLowerCase()) {
+            target.parentElement.parentElement.parentElement.remove();
+        }
+    
+    });
 }
 
 //Add a new book to the book grid.
 function displayBook(book) {
 
-    const extraPages = Math.floor(book.pages/50);
+    let extraPages = Math.floor(book.pages/50);
+    if (extraPages > 20) {extraPages = 20;}
 
     //Add a book container to the book grid.
     const bookGrid = document.getElementById('bookgrid');
@@ -90,6 +81,13 @@ function displayBook(book) {
     const bookTop = document.createElement('div');
     bookTop.classList.add('booktop');
     bookBox.appendChild(bookTop);
+
+    //Add edit book button.
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('editbtn');
+    editBtn.innerText = 'Edit';
+    bookTop.appendChild(editBtn);
+
 
     //Add book cover.
     const bookCover = document.createElement('img');
@@ -129,27 +127,159 @@ function displayBook(book) {
     bookRead.classList.add('bookinfo', 'bookread');
     bookRead.innerText = book.readAlert();
     thirdBox.appendChild(bookRead);
-    
 
     //Display extra pages based on book page count.
     for (let i = 0; i < extraPages; i++) {
         addPage(firstPage);
         addPageBottom(firstPage);
     }
+
+    editBtn.addEventListener('click', () => {
+        
+        console.log(book.title)
+
+        bookTop.style.backgroundColor = 'red';
+        const addBookPanel = document.getElementById('addbookpanel');
+        document.querySelector('legend').innerText = 'Edit Book';
+
+        addBookPanel.classList.toggle('addbookdisplay');
+        document.getElementById('titleinput').value = book.title;
+        document.getElementById('authorinput').value = book.author;
+        document.getElementById('pageinput').value = book.pages;
+        document.getElementById('readinput').value = book.read;
+        
+        submitBookBtn.addEventListener('click' , () => {
+          
+            if (validate()) {
+                book.delete();
+                console.log('book deleted')
+                book.title = document.getElementById('titleinput').value;
+                book.author = document.getElementById('authorinput').value;
+
+                
+                book.display();
+
+            }
+            
+
+        });
+        
+
+    });
 }
 
+
+//Returns last descendant of an element.
+function getLastDescendant(element) {
+    let descendants = Array.from(element.querySelectorAll('*'));
+    return descendants[descendants.length -1];
+}
+
+//Adds top page to the bottom of a book.
+function addPage(firstPage) {
+
+    let bottomPage = getLastDescendant(firstPage);
+
+    const newPage = document.createElement('div');
+    newPage.classList.add('page');
+    bottomPage.appendChild(newPage);
+}
+
+//Adds the lower page to the bottom of a book.
+function addPageBottom(firstPage) {
+
+    let bottomPage = getLastDescendant(firstPage);
+
+    const newPage = document.createElement('div');
+    newPage.classList.add('page', 'pagebottom');
+    bottomPage.appendChild(newPage);
+}
+
+//Clear inputs in the add book panel.
+function clearInputs() {
+    document.getElementById('titleinput').value = "";
+    document.getElementById('authorinput').value = "";
+    document.getElementById('pageinput').value = "";
+}
+
+//Validate inputs on form.
+function validate() {
+    let formValidity = 0;
+
+    let inputs = Array.from(document.querySelectorAll('input'));
+    inputs.forEach(input => {
+        if (input.validity.valid) {formValidity ++;}
+    });
+
+
+    if (formValidity === 3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Open or close the add book panel, and clear its inputs.
 const addBookBtn = document.querySelector('#addbookbtn');
 addBookBtn.addEventListener('click', () => {
-    const title = prompt('What is the title?');
-    const author = prompt('Who is the author?');
-    const pages = prompt('How many pages are there?');
-    const read = prompt('Have you read the book?');
-    const fuck = new Book(title, author, pages, read); 
-    addBook(fuck);
-    displayBook(fuck);
+    document.querySelector('legend').innerText = 'Add Book';
+    const addBookPanel = document.getElementById('addbookpanel');
+    addBookPanel.classList.toggle('addbookdisplay');
+    clearInputs();
 });
 
-myLibrary.forEach(book => {
-    displayBook(book);
+//Close book edt/add panel.
+const closeBtn = document.getElementById('closebtn');
+closeBtn.addEventListener('click', () => {
+    const addBookPanel = document.getElementById('addbookpanel');
+    addBookPanel.classList.toggle('addbookdisplay');
+    clearInputs();
 });
 
+
+//Add new book after submitting input.
+// function makeBookFromInput() {
+    
+//     let title = document.getElementById('titleinput').value;
+//     let author = document.getElementById('authorinput').value;
+//     let pages = document.getElementById('pageinput').value;
+//     const read = document.getElementById('readinput').value;
+
+//     if (validate()) { 
+//         const fuck = new Book(title, author, pages, read);
+//         addBook(fuck);
+//         displayBook(fuck);
+//         clearInputs();
+//         document.getElementById('addbookpanel').classList.toggle('addbookdisplay');
+//     }
+    
+// }
+
+
+
+const submitBookBtn = document.getElementById('submitbookbtn');
+
+// submitBookBtn.addEventListener('click' , () => {
+//     if (document.querySelector('legend').innerText === 'Add Book'){
+//         makeBookFromInput();
+//     }
+// });
+
+//Display default books.
+// myLibrary.forEach(book => {
+//     displayBook(book);
+// });
+
+// window.addEventListener('keydown', (key) => {
+//     if (key.code === 'Enter') {
+//         makeBookFromInput();
+//     }
+// });
+
+
+// window.addEventListener('click', () => {
+//     console.log(myLibrary);
+// });
+
+hatchet.display();
+testBook.display();
